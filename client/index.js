3a96698c843ee5d9418c577240a488ae048db606
Okada Shohei <okashoicircus0@gmail.com>
2018-05-26 17:30:57 +0900

    add client (by JavaScript)

---
+++ client/index.js
@@ -0,0 +1,10 @@
+const socket = new WebSocket('ws://localhost:8080/');
+
+socket.addEventListener('open', function (event) {
+    const message = {msg: 'hello world!'};
+    socket.send('echo ' + JSON.stringify(message));
+});
+
+socket.addEventListener('message', function (event) {
+    console.log('Message from server', event.data);
+});
