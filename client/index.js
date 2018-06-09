4835ed2a1263e5fe9ff8b7d2767b60c60d832cb9
Okada Shohei <okashoicircus0@gmail.com>
2018-06-09 18:57:22 +0900

    rewrite codes for gorilla/websocket

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ const socket = new WebSocket('ws://localhost:8080/');
 
 socket.addEventListener('open', function (event) {
     const message = {msg: 'hello world!'};
-    socket.send('echo ' + JSON.stringify(message));
+    socket.send(JSON.stringify(message));
 });
 
 socket.addEventListener('message', function (event) {
--- server/src/main.go
+++ server/src/main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
-	"github.com/trevex/golem"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(*http.Request) bool {
+		return true
+	},
+}
+
 func main() {
-	http.HandleFunc("/", createRouter().Handler())
+	http.HandleFunc("/", handle)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func createRouter() *golem.Router {
-	r := golem.NewRouter()
-	r.On("echo", echo)
-	return r
-}
-
-func echo(conn *golem.Connection, data *message) {
-	conn.Emit("echo", data)
-}
-
-type message struct {
-	Msg string `json:"msg"`
+func handle(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+	defer conn.Close()
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("received: %s", message)
+		err = conn.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
